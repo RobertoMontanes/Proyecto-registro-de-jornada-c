f760f84642013ee8757888bd050561bd96e94603
Roberto Montañés Ramos <rmr15092004@gmail.com>
2025-11-24 17:57:13 +0100

    Primera subida

---
+++ Proyecto_API_Rest_Spring_Boot_C/src/main/java/com/salesianos/triana/proyecto_api_rest_spring_boot_c/ProyectoApiRestSpringBootCApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.proyecto_api_rest_spring_boot_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiRestSpringBootCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiRestSpringBootCApplication.class, args);
+	}
+
+}
---
+++ Proyecto_API_Rest_Spring_Boot_C/src/test/java/com/salesianos/triana/proyecto_api_rest_spring_boot_c/ProyectoApiRestSpringBootCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.proyecto_api_rest_spring_boot_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApiRestSpringBootCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
